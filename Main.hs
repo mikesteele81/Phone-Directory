874ef819c38d7e638ec0f1cbf50c3cd541558a9e
Mike Steele <mikste@.energy.wsu.edu>
2009-07-10 13:12:19 -0700

    Title displayed on blank page

--- Main.hs
+++ Main.hs
@@ -3,13 +3,20 @@ module Main where
 import Graphics.PDF
 
 units_per_inch = 72
-page_width = units_per_inch * 85 `div` 10
-page_height = units_per_inch * 11
+page_width     = units_per_inch * 85 `div` 10
+page_height    = units_per_inch * 11
+page_margin    = units_per_inch `div` 4
+
+title_inset    = units_per_inch * 175 `div` 100
+
+font_title     = PDFFont Helvetica 20
 
 main = runPdf "test.pdf" standardDocInfo (PDFRect 0 0 page_width page_height) pdf
     where
       pdf = do
         p <- addPage Nothing
         drawWithPage p $ drawText $
-          sequence $ replicate 10 $
-            text (PDFFont Helvetica 10) 10 10 (toPDFString "ABC")
+                     text font_title
+                              (fromIntegral title_inset )
+                              (fromIntegral (page_height - page_margin) - getHeight font_title)
+                              (toPDFString "PHONE DIRECTORY")
