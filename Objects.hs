3f6c07ac478ff45845ea9b2720787f7c9319ed50
Mike Steele <mikste@IS018970V06.energy.wsu.edu>
2009-07-22 11:36:59 -0700

    Moved ContactInfo into its own module.  Used Modoid instance to increase ease of reading.

--- Objects.hs
+++ Objects.hs
@@ -1,47 +1,12 @@
-{-# LANGUAGE ExistentialQuantification #-}
 module Objects
     where
 
 import Data.List (sort)
 import Text.JSON
 
+import ContactInfo
 import Name
     
-data ContactInfo name = ContactInfo
-    { cName :: name
-    , cPhone    :: String
-    -- for the purposes of sorting.  Higher numbers sort first.
-    , cPriority :: Int
-    } deriving (Eq)
-
-instance (JSON a) => JSON (ContactInfo a) where
-    readJSON (JSObject o) =
-        do
-          name <- valFromObj "name" o
-          phone <- valFromObj "phone" o
-          priority <- valFromObj "priority" o
-          return $ ContactInfo name phone priority
-    readJSON _ = Error "Could not parse ContactInfo JSON object."
-    showJSON (ContactInfo n p pr) =
-        showJSON $ toJSObject $
-                     [ ("name", showJSON n )
-                     , ("phone", showJSON p)
-                     , ("priority", showJSON pr) ]
-
-instance forall a. (Ord a) => Ord (ContactInfo a) where
-    compare l r =                     
-        -- priority descending
-        case compare (cPriority r) (cPriority l) of
-          -- name ascending
-          EQ -> case compare (cName l) (cName r) of
-                  -- phone ascending
-                  EQ -> compare (cPhone l) (cPhone r)
-                  x  -> x
-          x  -> x
-          
-instance Functor ContactInfo where
-    f `fmap` x = x { cName = (f . cName) x }
-
 -- An organization persons are a part of
 data Organization name
     = Organization
@@ -115,7 +80,7 @@ testOrg =
               , cPriority = 1 }
     , oContacts =
         [ ContactInfo
-          { cName = FirstLast "Brett" "Anderson"
+          { cName = FirstLast "Brett" "Zeldman"
           , cPhone = "111-111-1112"
           , cPriority = 1 }
         , ContactInfo
