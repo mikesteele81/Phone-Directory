24750c3606df14e993793b8cf32c0a552a111569
Mike Steele <mikste@IS018970V06.energy.wsu.edu>
2009-07-22 13:02:02 -0700

    commented out main2 and got rid of warnings in Main.hs

--- Main.hs
+++ Main.hs
@@ -5,8 +5,9 @@ import System.IO
 import Text.JSON
 import qualified Text.JSON.Pretty as JP
 
-import Objects
+--import Objects
 
+units_per_inch :: Int
 units_per_inch = 72
                  
 page_width, page_height :: Int
@@ -17,21 +18,27 @@ page_margin, title_inset, title_rise :: PDFFloat
 page_margin    = fromIntegral units_per_inch / 4.0
 title_inset    = fromIntegral units_per_inch * 1.75
 title_rise     = fromIntegral units_per_inch * 10.5
+
+title_string :: PDFString
 title_string   = toPDFString "PHONE DIRECTORY"
-                 
+
+date_inset, date_rise :: PDFFloat                 
 date_inset     = page_margin
 date_rise      = fromIntegral units_per_inch * 10.0
+date_string :: PDFString
 date_string    = toPDFString "Revised: 07/01/09"
 
+mode_inset, mode_rise :: PDFFloat
 mode_inset     = title_inset
 mode_rise      = date_rise
+mode_string :: PDFString
 mode_string    = toPDFString "(Sorted by Location and then First Name)"
 
+font_title, font_normal :: PDFFont
 font_title     = PDFFont Helvetica 20
 font_normal    = PDFFont Helvetica 10
 
-
-
+main :: IO ()
 main = do
   putStrLn "Opening test.json..."
   res <- withFile "test.json" ReadMode grabJSON
@@ -41,20 +48,21 @@ main = do
         do
           putStrLn "Done!"
           putStrLn $ show $ JP.pp_value x
-  
-main2 = runPdf "test.pdf" standardDocInfo (PDFRect 0 0 page_width page_height) $
-       do
-         p <- addPage Nothing
-         drawWithPage p $
-          do
-            drawText $ text font_title title_inset title_rise title_string
-            drawText $ text font_normal date_inset date_rise date_string
-            drawText $ text font_normal mode_inset mode_rise mode_string
-            beginPath (300 :+ 300)
-            lineto (350 :+ 320)
-            strokePath
-            closePath
-            drawText $ text font_normal 0.0 0.0 $ toPDFString "!"
+
+--main2 :: IO ()  
+--main2 = runPdf "test.pdf" standardDocInfo (PDFRect 0 0 page_width page_height) $
+--       do
+--         p <- addPage Nothing
+--         drawWithPage p $
+--          do
+--            drawText $ text font_title title_inset title_rise title_string
+--            drawText $ text font_normal date_inset date_rise date_string
+--            drawText $ text font_normal mode_inset mode_rise mode_string
+--            beginPath (300 :+ 300)
+--            lineto (350 :+ 320)
+--            strokePath
+--            closePath
+--            drawText $ text font_normal 0.0 0.0 $ toPDFString "!"
 
 grabJSON :: Handle -> IO (Result JSValue)
 grabJSON h = do
