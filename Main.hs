18912a8b737ef7efd04293b4c8b11df41cd3fb39
Mike Steele <mikste@IS018970V06.energy.wsu.edu>
2009-07-23 11:33:04 -0700

    Program now grabs data from test.json rather than Objects.hs

--- Main.hs
+++ Main.hs
@@ -3,31 +3,30 @@ module Main where
 import Graphics.PDF
 import System.IO
 import Text.JSON
---import qualified Text.JSON.Pretty as JP
+import qualified Text.JSON.Pretty as JP
 
 import Constants
 import Document
-import Objects
+import Name
 
---main1 :: IO ()
---main1 = do
---  putStrLn "Opening test.json..."
---  res <- withFile "test.json" ReadMode grabJSON
---  case res of
---    Error s -> putStrLn $ "Error: " ++ s
---    Ok x    ->
---        do
---          putStrLn "Done!"
---          putStrLn $ show $ JP.pp_value x
+main :: IO ()
+main = do
+  putStrLn "Opening test.json..."
+  res <- withFile "test.json" ReadMode grabJSON
+  case res of
+    Error s -> putStrLn $ "Error: " ++ s
+    Ok x    ->
+        do
+          putStrLn "Done!"
+          putStrLn $ show $ JP.pp_value x
+          case (readJSON x :: Result (Document Name)) of
+            Error s -> putStrLn $ s
+            Ok x' -> runPdf "test.pdf" standardDocInfo
+                    (PDFRect 0 0 page_width page_height) $
+                    do renderDoc x'
 
-main :: IO ()  
-main =
-    runPdf "test.pdf" standardDocInfo (PDFRect 0 0 page_width page_height) $
-    do
-      renderDoc testDoc
-
---grabJSON :: Handle -> IO (Result JSValue)
---grabJSON h = do
---  hStr <- hGetContents h
---  putStrLn $ hStr
---  return $ decodeStrict hStr
+grabJSON :: Handle -> IO (Result JSValue)
+grabJSON h = do
+  hStr <- hGetContents h
+  putStrLn $ hStr
+  return $ decodeStrict hStr
