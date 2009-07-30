9c6bad30313c7c0c8b57fee957e098d5c71106e9
Mike Steele <mikste@IS018970V06.energy.wsu.edu>
2009-07-30 12:42:43 -0700

    Moved tree2doc to GUI.hs

--- GUI.hs
+++ GUI.hs
@@ -1,4 +1,23 @@
 module GUI where
-    
+
+import Control.Applicative
+import Graphics.UI.WX
+import Graphics.UI.WXCore hiding ( Document )
+
+import Document
+import Name
+import Organization
+
 file_types_selection :: [(String, [String])]
 file_types_selection = [("JSON", ["*.json"])]
+
+-- | Create a Document object based on the tree.      
+tree2Doc :: TreeCtrl a -> IO (Maybe (Document Name))
+tree2Doc tc = do
+  root <- treeCtrlGetRootItem tc
+  orgs <- treeCtrlWithChildren tc root $ \itm -> do
+    orgCI <- unsafeTreeCtrlGetItemClientData tc itm
+    contacts <- treeCtrlWithChildren tc itm $ \itm' -> do
+      unsafeTreeCtrlGetItemClientData tc itm'
+    return $ Organization <$> orgCI <*> sequence contacts
+  return $ Document "TODO - add date" <$> sequence orgs
--- Main.hs
+++ Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Control.Applicative
 import Control.Monad
 import Data.Maybe (fromJust)
 import Graphics.PDF
@@ -204,17 +203,6 @@ populateTree tc doc =
       mapM_ (populateOrg root) $ dOrganizations doc
       treeCtrlSelectItem tc root
       treeCtrlExpand tc root
-
--- | Create a Document object based on the tree.      
-tree2Doc :: TreeCtrl a -> IO (Maybe (Document Name))
-tree2Doc tc = do
-  root <- treeCtrlGetRootItem tc
-  orgs <- treeCtrlWithChildren tc root $ \itm -> do
-    orgCI <- unsafeTreeCtrlGetItemClientData tc itm
-    contacts <- treeCtrlWithChildren tc itm $ \itm' -> do
-      unsafeTreeCtrlGetItemClientData tc itm'
-    return $ Organization <$> orgCI <*> sequence contacts
-  return $ Document "TODO - add date" <$> sequence orgs
 
 parseOpts :: [String] -> IO Options
 parseOpts argv = 
