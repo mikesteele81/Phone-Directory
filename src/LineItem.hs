c0a38d498d68ccd3f126d7459b03f010b1b1924e
Michael Steele <mikesteele81@gmail.com>
2009-08-12 08:27:21 -0700

    added a few comments.

--- src/LineItem.hs
+++ src/LineItem.hs
@@ -78,12 +78,14 @@ drawLineItem (LineItem l r i) =
          displayText r
          textStart (textWidth font_normal r - line_item_width) 0
       return (x :+ (y - line_item_leading))
+-- Horizontal line along the bottom of the drawing area.
 drawLineItem Divider = do
   (x :+ y) <- ask
   let x' = x + col_width
       y' = y - line_item_leading
   lift $ stroke (Line x y' x' y')
   return (x :+ (y - line_item_leading))
+-- Only used to fill the last column.
 drawLineItem Blank = asks (+(0 :+ (-line_item_leading)))
 
 -- |Draw a collection of LineItem objects with a box around it.
