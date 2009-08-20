9f0fe4a641cf96cbfe76cb5ec2d3b75e9732855a
Michael Steele <mikesteele81@gmail.com>
2009-08-20 12:54:56 -0700

    SingleNames now save as a JSString rather than JSObject.

--- src/Name.hs
+++ src/Name.hs
@@ -79,10 +79,9 @@ instance JSON Name where
         <|> SingleName <$> valFromObj "name" o
     readJSON v = Error $ "Expected JSObject, but " ++ (show . pp_value) v
         ++ " found while parsing a name."
-    showJSON n = makeObj
-        $ case n of
-            FirstLast f l -> [ ("first", showJSON f), ("last" , showJSON l) ]
-            SingleName sn -> [ ("name" , showJSON sn) ]
+    showJSON n = case n of
+        FirstLast f l -> makeObj [ ("first", showJSON f), ("last" , showJSON l) ]
+        SingleName sn -> showJSON sn
                    
 instance JSON FirstSortedName where
     readJSON n = FirstSortedName <$> readJSON n
