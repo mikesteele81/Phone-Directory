b6b43d617e2849cdd9f7c9149390feea0e1f6e75
Michael Steele <mikesteele81@gmail.com>
2009-10-19 17:00:12 -0700

    Refreshed copyright notices.

--- src/PageProperties.hs
+++ src/PageProperties.hs
@@ -1,4 +1,25 @@
-module PageProperties where
+{- This file is part of PhoneDirectory.
+   Copyright (C) 2009 Michael Steele
+
+   PhoneDirectory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   PhoneDirectory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with PhoneDirectory.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+module PageProperties
+    ( Layout (..)
+    , PageProperties (..)
+    , mkPageProperties
+    ) where
 
 import Control.Applicative
 import Control.Monad.Error
