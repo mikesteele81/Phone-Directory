e7910fceeec63896ededafdff1d5c6a88f418b26
Michael Steele <mikesteele81@gmail.com>
2009-10-28 13:42:49 -0700

    Added GUIConstants.hs.  This file has been needed for a few commits.

---
+++ src/GUIConstants.hs
@@ -0,0 +1,32 @@
+{- This file is part of PhoneDirectory.
+   Copyright (C) 2009 Michael Steele
+
+   PhoneDirectory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   PhoneDirectory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with PhoneDirectory.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+module GUIConstants where
+
+-- |The number of pixels between controls that are grouped together.
+ctrlPadding :: Int
+ctrlPadding = 4
+
+-- |The number of pixels between controls and their labels.
+lblPadding :: Int
+lblPadding = 3
+
+-- |The number of pixels between the window border or vertical/horizontal
+-- spacers and internal controls.
+winPadding :: Int
+winPadding = 7
+
