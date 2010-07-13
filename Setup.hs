3ffa6ec1fe51e3199587cb54f94192349e24f764
Michael Steele <mikesteele81@gmail.com>
2010-07-13 15:14:19 -0700

    Simple.hs and .cabal edits to allow \'cabal test\' to run a test suite.  It prints "Hello, World" for now.

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,15 @@
 #!/usr/bin/env runhaskell
+import qualified Distribution.PackageDescription as P
 import Distribution.Simple
-main = defaultMainWithHooks defaultUserHooks
+import qualified Distribution.Simple.LocalBuildInfo as L
+import System
+import System.FilePath
+
+main :: IO ()
+main = defaultMainWithHooks
+       $ simpleUserHooks { runTests = tests }
+
+tests :: Args -> Bool -> P.PackageDescription -> L.LocalBuildInfo -> IO ()
+tests _ _ _ lbi = system testprog >> return ()
+  where
+    testprog = (L.buildDir lbi) </> "test" </> "test"
